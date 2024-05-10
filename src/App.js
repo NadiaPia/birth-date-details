79f32563aa2128b147e7e51864ed2bd37ab10b5e
NadiaPia <206i417@gmail.com>
2024-05-09 23:01:18 -0700

    added time zone

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import heroImage from "./images/tree1.jpg";
+import TimezoneSelect from "react-timezone-select";
 
 function App() {
   const [year, setYear] = useState(0);
@@ -8,24 +9,26 @@ function App() {
   const [hours, setHours] = useState("");
   const [min, setMin] = useState("");
   const [inputDate, setInputDate] = useState([]);
+  const [timeZone, setTimeZone] = useState("")
+
+  const [selectedTimezone, setSelectedTimezone] = useState("");
 
   //const myBirth = new Date("oct 03, 1987 00:00:00 GMT+00:00");
 
   const currentDateMls = new Date().getTime();
+
   const randomDateMls = new Date(
-    `${month} ${day}, ${year} ${hours}:${min}:00 GMT+00:00`
-  ).getTime();
-
-  //console.log("currentDate", currentDate)
-  //console.log("currentDateMls", currentDateMls);
-  //console.log("birth", birth)
-  //console.log("myBirth", myBirth)
-  //console.log("randomDateMls", randomDateMls);
-
+   `${month} ${day}, ${year} ${hours}:${min}:00 ${timeZone ? timeZone : 'GMT+00:00'}`
+  );
+  
   function showData(event) {
-    const dateString = event.target.value; //2013-04-05T21:45
+    const dateString = event.target.value; //2013-04-05T21:45    
     const dateArr = dateString.split(/[-T:]/); //['2024', '06', '07', '07', '24']
     setInputDate(dateArr);
+  }
+
+  function showTimezone(event) {  
+    setSelectedTimezone(event); //console.log("event", event) {value: 'America/Phoenix', label: '(GMT-7:00) Arizona', offset: -7, abbrev: 'MST', altName: 'Mountain Standard Time'}
   }
 
   function handleCount() {
@@ -34,30 +37,46 @@ function App() {
     setDay(inputDate[2]);
     setHours(inputDate[3]);
     setMin(inputDate[4]);
-  }
+
+    const timezoneString = (selectedTimezone.label).split(/[""()]/)[1];
+    setTimeZone(timezoneString);
+  }    
 
   return (
-    <div class="flex items-center justify-center h-screen bg-zinc-700 pb-2">
-      <div class="bg-zinc-800 p-2 mx-6 rounded-2xl w-96">
+    <div className="flex items-center justify-center min-h-screen bg-zinc-700 pb-2">
+      <div className="bg-zinc-800 p-2 mx-6 rounded-2xl w-96">
         {/* -------------Flex Container---------------------- */}
-        <div class="flex flex-col rounded-l-xl">
-          <img src={heroImage} alt="" class=" rounded-b-none object-cover rounded-xl h-80" />
+        <div className="flex flex-col rounded-l-xl">
+          <img
+            src={heroImage}
+            alt=""
+            className=" rounded-b-none object-cover rounded-xl h-80"
+          />
 
-          <div class="p-6">
-            <h2 class="font-serif text-xl font-medium text-center text-white">
+          <div className="p-6">
+            <h2 className="font-serif text-xl font-medium text-center text-white">
               Choose your birth date:
             </h2>
 
             <div>
-              <div class="flex flex-col mt-5 space-y-4">
+              <div className="flex flex-col mt-5 space-y-4">
                 <input
                   type="datetime-local"
                   onChange={showData}
-                  class="w-80 text-base text-white bg-transparent border border-zinc-600 py-2 px-2 outline-none rounded-md"
+                  className="w-80 text-base text-white bg-transparent border border-zinc-600 py-2 px-2 outline-none rounded-md"
                 />
 
+                <div >
+                  <TimezoneSelect
+                    value={selectedTimezone}
+                    onChange={showTimezone}
+                    style={{background: 'red'}}     
+                  />
+                </div>
+                
+
                 <button
-                  class="px-5 py-3 text-s font-medium rounded-md text-zinc-800 bg-lime-500 hover:bg-lime-700 hover:text-white duration-500"
+                  className="px-5 py-3 text-s font-medium rounded-md text-zinc-800 bg-lime-500 hover:bg-lime-700 hover:text-white duration-500"
                   onClick={handleCount}
                 >
                   Count
@@ -66,10 +85,9 @@ function App() {
             </div>
 
             {min && (
-              <div class="max-w-xs my-2 text-s leading-7 tracking-wide text-start text-white">
-                {/* <div>{currentDateMls - randomDateMls}</div> */}
+              <div className="max-w-xs my-2 text-s leading-7 tracking-wide text-start text-white">
 
-                <p class="font-serif text-l font-medium text-center text-white">
+                <p className="font-serif text-l font-medium text-center text-white">
                   Your age in
                 </p>
 
@@ -95,7 +113,7 @@ function App() {
                 <div>
                   <span>weeks: </span>
                   {Math.floor(
-                    ((currentDateMls - randomDateMls) / 1000 / 3600 / 24) / 7
+                    (currentDateMls - randomDateMls) / 1000 / 3600 / 24 / 7
                   )}
                 </div>
 
